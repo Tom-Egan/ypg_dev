0c1b27014b2846f185492b90cb7a21b19a4008ab
Tom-Egan <info@tom-egan.com>
2017-05-21 18:14:25 -0700

    Mobile navigation menu items

---
+++ js/nav_toggle.js
@@ -0,0 +1,106 @@
+function toggleHamburger() {
+    // mobile navigation menu wrapper
+    var mobileNav = document.getElementById('ypg_nav_mobile');
+    var hamburger = document.getElementById('hamburger');
+    // main navbar bg, used to toggle opacitity on state changes
+    var opaqueNav = document.getElementById('opaqueNav');
+    var ypgLogo = document.getElementById('ypgLogo');
+
+    var ham_line1 = document.getElementById('ham_line1');
+    var ham_line2 = document.getElementById('ham_line2');
+    var ham_line3 = document.getElementById('ham_line3');
+
+    var mobile_phone= document.getElementById("mobile_phone_btn");
+
+    // if mobile nav is display, hide it on button press
+    if (mobileNav.style.width === '100%') {
+    	// hide mobile navigation
+        mobileNav.style.width = '0';
+        // convert hamburger button back to absolute
+        hamburger.style.position = 'absolute';
+        ypgLogo.style.opacity = '1';
+        ypgLogo.style.left = '0';
+
+        ham_line1.style.transform = 'rotate(0deg)';
+        ham_line1.style.marginTop = '0';
+        ham_line1.style.background = 'rgba(0,0,0,0.7)';
+        ham_line3.style.marginTop = '0';
+        ham_line2.style.opacity = '1';
+        ham_line2.style.width = '100%';
+        ham_line3.style.transform = 'rotate(0deg)';
+        ham_line3.style.background = 'rgba(0,0,0,0.7)';
+
+        mobile_phone.style.opacity = '0';
+
+        // if mobile nav is not active, revert back to 65% opacity
+        opaqueNav.style.background  = 'rgba(255,255,255,0.65)';
+        // css transition on state change
+        mobileNav.style.WebkitTransition = 'ease-out 0.28s';
+        mobileNav.style.MozTransition = 'ease-out 0.28s';
+        mobileNav.style.transition = 'ease-out 0.28s';
+        opaqueNav.style.WebkitTransition = 'ease-out 0.24s';
+        opaqueNav.style.MozTransition = 'ease-out 0.24s';
+        opaqueNav.style.transition = 'ease-out 0.24s';
+        ham_line1.style.WebkitTransition = 'ease-out 0.24s';
+        ham_line1.style.MozTransition = 'ease-out 0.24s';
+        ham_line1.style.transition = 'ease-out 0.24s';
+        ham_line2.style.WebkitTransition = 'ease-out 0.24s';
+        ham_line2.style.MozTransition = 'ease-out 0.24s';
+        ham_line2.style.transition = 'ease-out 0.24s';
+        ham_line3.style.WebkitTransition = 'ease-out 0.24s';
+        ham_line3.style.MozTransition = 'ease-out 0.24s';
+        ham_line3.style.transition = 'ease-out 0.24s';
+        mobile_phone.style.WebkitTransition = 'ease-out 0.24s';
+        mobile_phone.style.MozTransition = 'ease-out 0.24s';
+        mobile_phone.style.transition = 'ease-out 0.24s';
+        ypgLogo.style.WebkitTransition = 'ease-out 0.3s';
+        ypgLogo.style.MozTransition = 'ease-out 0.3s';
+        ypgLogo.style.transition = 'ease-out 0.3s';
+
+    // else if mobile nav is hidden, display it
+    } else {
+    	// display mobile navigation
+        mobileNav.style.width = '100%';
+        // convert hamburger button to fixed
+        hamburger.style.position = 'fixed';
+        // if mobile nav is active, make navbar opaque
+        opaqueNav.style.background  = 'rgba(255,255,255,0)'
+        ypgLogo.style.opacity = '0';
+        ypgLogo.style.left = '-14px';
+
+        ham_line1.style.transform = 'rotate(-45deg)';
+        ham_line1.style.background = 'rgba(255,255,255,1)';
+        ham_line1.style.marginTop = '4px';
+        ham_line2.style.opacity = '0';
+        ham_line2.style.width = '0%';
+        ham_line3.style.transform = 'rotate(45deg)';
+        ham_line3.style.marginTop = '-14px';
+        ham_line3.style.background = 'rgba(255,255,255,1)';
+
+        mobile_phone.style.opacity = '1.0';
+
+        ham_line1.style.WebkitTransition = 'ease-in 0.24s';
+        ham_line1.style.MozTransition = 'ease-in 0.24s';
+        ham_line1.style.transition = 'ease-in 0.24s';
+        ham_line2.style.WebkitTransition = 'ease-in 0.2s';
+        ham_line2.style.MozTransition = 'ease-in 0.2s';
+        ham_line2.style.transition = 'ease-in 0.2s';
+        ham_line3.style.WebkitTransition = 'ease-in 0.24s';
+        ham_line3.style.MozTransition = 'ease-in 0.24s';
+        ham_line3.style.transition = 'ease-in 0.24s';
+
+        // css transition on state change
+        mobileNav.style.WebkitTransition = 'ease-out 0.28s';
+        mobileNav.style.MozTransition = 'ease-out 0.28s';
+        mobileNav.style.transition = 'ease-out 0.28s';
+        mobile_phone.style.WebkitTransition = 'ease-out 0.4s';
+        mobile_phone.style.MozTransition = 'ease-out 0.4s';
+        mobile_phone.style.transition = 'ease-out 0.4s';
+        opaqueNav.style.WebkitTransition = 'ease-in 0.2s';
+        opaqueNav.style.MozTransition = 'ease-in 0.2s';
+        opaqueNav.style.transition = 'ease-in 0.2s';
+        ypgLogo.style.WebkitTransition = 'ease-in 0.24s';
+        ypgLogo.style.MozTransition = 'ease-in 0.24s';
+        ypgLogo.style.transition = 'ease-in 0.24s';
+    }
+} // end of toggleHamburger function
