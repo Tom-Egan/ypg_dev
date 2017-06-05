ae845b83681be81f007a700c4ddeafe004de5ed3
Tom-Egan <info@tom-egan.com>
2017-06-04 23:46:24 -0700

    Service Tiles Roughed-in

---
+++ modules/footer.php
@@ -0,0 +1,145 @@
+<!-- footer -->
+<div class="footer_bg">
+	<div class="ypg_wrap">
+		<!-- company -->
+		<div class="col-md-3 footer_side_box">
+			<h4>Company</h4>
+			<ul>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Careers</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Portfolio</a></li>
+			</ul>
+		</div>
+
+		<!-- services -->
+		<div class="col-md-6">
+			<!-- row 1 -->
+			<div class="row">
+				<!-- landscape -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Landscape</a>
+				</div>
+				<!-- lawn and turf -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Lawn and Turf</a>
+				</div>
+				<!-- tree service -->
+				<div class="col-md-4 footer_grid_box right_border">
+					<a href="">Tree Service</a>
+				</div>
+			</div> 
+			<!-- row 2 -->
+			<div class="row">
+				<!-- hardscape -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Hardscape</a>
+				</div>
+				<!-- concrete -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Concrete</a>
+				</div>
+				<!-- landscape design -->
+				<div class="col-md-4 footer_grid_box right_border">
+					<a href="">Landscape Design</a>
+				</div>
+			</div>
+			<!-- row 3 -->
+			<div class="row">
+				<!-- maintenance -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Maintenance</a>
+				</div>
+				<!-- plant health care -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Plant Health Care</a>
+				</div>
+				<!-- grading and drainage -->
+				<div class="col-md-4 footer_grid_box right_border">
+					<a href="">Grading and Drainage</a>
+				</div>
+			</div>
+			<!-- row 4 -->
+			<div class="row">
+				<!-- mosquito and tick control -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Mosquito and Tick Control</a>
+				</div>
+				<!-- firewood, logs, and mulch -->
+				<div class="col-md-4 footer_grid_box">
+					<a href="">Firewood, Logs, and Mulch</a>
+				</div>
+				<!-- snow and ice management -->
+				<div class="col-md-4 footer_grid_box right_border">
+					<a href="">Snow and Ice Management</a>
+				</div>
+			</div> <!-- end of row 4 -->
+		</div> <!-- end of col-md-6 -->
+
+		<!-- contact -->
+		<div class="col-md-3 footer_side_box right_align">
+			<h4>Contact</h4>
+			<ul>
+				<li><a href="#">(262) 470-3829</a></li>
+				<li><a href="#">YourPersonalGardenerLLC@gmail.com</a></li>
+				<li><a href="#">Facebook</a></li>
+				<li><a href="#">Google+</a></li>
+			</ul>
+		</div> 
+		<!-- awards and affiliates -->
+		<div class="row_buffer">
+			<div class="row awards_row">
+				<div class="col-md-12">
+					<h4>Awards and Affiliates</h4>
+				</div>
+			<!-- 2017 expertise award -->
+				<div class="col-sm-2"><img title="Expertise.com's Top 25 Best Milwaukee Landscapers for 2017" alt="Expertise.com's Top 25 Best Milwaukee Landscapers for 2017" height="150" width="150" class="img-responsive" src="images/affiliates-awards/award_expertise2017.svg"></div>
+				<!-- 2016 expertise award -->
+				<div class="col-sm-2"><img title="Expertise.com's Top 25 Best Milwaukee Landscapers for 2016" alt="Expertise.com's Top 25 Best Milwaukee Landscapers for 2016" height="150" width="150" class="img-responsive" src="images/affiliates-awards/award_expertise2016.svg"></div>
+				<!-- 2015 gold leaf award -->
+				<div class="col-sm-2"><img title="2015 Gold Leaf Award" alt="2015 Gold Leaf Award" height="150" width="180" class="img-responsive" src="images/affiliates-awards/award_gold_leaf.png"></div>
+				<!-- simply the best 2016 -->
+				<div class="col-sm-2"><img title="2016 Mukwonago Area's 'Simply the Best' Landscape and Lawn Maintenance Company" alt="2016 Mukwonago Area's 'Simply the Best' Landscape and Lawn Maintenance Company" height="130" width="106" class="img-responsive" src="images/affiliates-awards/award_best_2016_130.png"></div>
+				<!-- simply the best 2015 -->
+				<div class="col-sm-2"><img title="2015 Mukwonago Area's 'Simply the Best' Landscape and Lawn Maintenance Company" alt="2015 Mukwonago Area's 'Simply the Best' Landscape and Lawn Maintenance Company" height="130" width="106" class="img-responsive" src="images/affiliates-awards/award_best_2015_130.png"></div>
+				<!-- simply the best 2014 -->
+				<div class="col-sm-2"><img title="2014 Mukwonago Area's 'Simply the Best' Landscape and Lawn Maintenance Company" alt="2014 Mukwonago Area's 'Simply the Best' Landscape and Lawn Maintenance Company" height="130" width="105" class="img-responsive" src="images/affiliates-awards/award_best_2014_130.png"></div>
+			</div>
+
+			<div class="row awards_row">
+				<!-- wisconsin landscape contractor association -->
+				<div class="col-sm-2"><img title="Wisconsin Landscape Contractors Association" alt="Wisconsin Landscape Contractors Association" height="130" width="357" class="img-responsive wlca" src="images/affiliates-awards/afil_wlca_130.png"></div>
+				<!-- mukwonago chamber of commerce -->
+				<div class="col-sm-2"><img title="Mukwonago Area Chamber of Commerce and Tourism Center" alt="Mukwonago Area Chamber of Commerce and Tourism Center" height="130" width="130" class="img-responsive" src="images/affiliates-awards/afil_muk_coc_130.png"></div>
+				<!-- master gardener -->
+				<div class="col-sm-2"><img title="UW-Extension Master Gardener Program" alt="UW-Extension Master Gardener Program" height="130" width="130" class="img-responsive" src="images/affiliates-awards/afil_master_gardener_130.png"></div>
+				<!-- pesticide applicator -->
+				<div class="col-sm-2"><img title="UW-Extension Pesticide Applicator Training Program" alt="UW-Extension Pesticide Applicator Training Program" height="130" width="130" class="img-responsive" src="images/affiliates-awards/afil_pat_130.png"></div>
+				<!-- wisconsin arborist association -->
+				<div class="col-sm-2"><img title="Wisconsin Arborist Association" alt="Wisconsin Arborist Association" height="130" width="130" class="img-responsive" src="images/affiliates-awards/afil_waa_130.png"></div>
+				<!-- isa member -->
+				<div class="col-sm-2"><img title="ISA Member" alt="ISA Member" height="129" width="82" class="img-responsive" src="images/affiliates-awards/afil_isa_130.png"></div>
+			</div>
+		</div> <!-- end of row -->
+	</div> <!-- end of ypg_wrap -->
+
+	<!-- relative/clear fix -->
+	<div class="rel_fix"></div>
+
+</div> <!-- end of footer_bg -->
+
+<div class="footer_bg2">
+	<div class="ypg_wrap">
+		<!-- <div class="col-md-6">
+			<p>&copy; 2017 Your Personal Gardener, LLC. All rights reserved.</p>
+		</div>
+
+		<div class="col-md-6">
+			<p>Designed and Built by Tom Egan</p>
+		</div> -->
+		<p class="float_left">&copy; 2017 Your Personal Gardener, LLC. All rights reserved.</p>
+
+		<p class="float_right">Designed and handbuilt by <a title="The YPG website was designed and built from scratch by Tom Egan, come take a look at some of his other projects." alt="This website was designed and built from scratch by Tom Egan, come take a look at some of his other projects." target="_blank" href="http://tom-egan.com">Tom Egan</a>.</p>
+	</div>
+	<div class="rel_fix"></div>
+</div>
